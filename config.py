7fe447552a85bd045bfd963cdbd30b868d8ba698
ShityBrain <65502219+ShityBrainOfficial@users.noreply.github.com>
2020-05-28 13:13:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,27 +8,23 @@ class ChatGroup:
     self.chatlist = chatlist;
     self.delete_before_days = delete_before_days;
   chatlist = [];
-  delete_before_days = 0;
+  delete_before_days = 1;
 
 # API id and hash obtained during setup
-tg_api_id = 12345;
-tg_api_hash = "abcd";
+tg_api_id = 1054125;
+tg_api_hash = "62e934254fecb43c753975e766d5a150";
 
 # List of chats for a group
 
 listA = [
-  "https://t.me/joinchat/abcdef",
-  -12345
+  "https://t.me/joinchat/RE3FvVY695c1_yLATpvzmA",
+  -1001446705047
 ];
 
-listB = [
-  "@abcdef"
-];
 
 # List of GroupChat targets
 # Taking lists as first argument and days to not delete as second
 
 targets = [
-  ChatGroup(listA, 14),
-  ChatGroup(listB, 2)
+  ChatGroup(listA, 1),
 ]
